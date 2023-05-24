57bcf55f24fe16c4d7cdbf7ba0cdff15f2b787da
@code <@code>
2023-05-24 07:15:43 +0700

    renderingComponent

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Person from "./person";
 
-const myFirstElement = <h1>Hello Mitsuya</h1>
+function Mitsuya() {
+  return <h1>Hello Mitsuya</h1>
+} function Draken() {
+  return (
+    <>
+      <h2> Hello Draken</h2 >
+      <Mitsuya />
+    </>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<Draken />);
