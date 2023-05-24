5059c72891932fa4d90e06a6d2350ae2e7112611
@code <@code>
2023-05-25 04:21:05 +0700

    state-components

--- src/components/person.js
+++ src/components/person.js
@@ -4,9 +4,34 @@ import React from "react";
 class Person extends React.Component {
     constructor() {
         super();
+        this.state = {
+            name: 'Mitsuya',
+            age: 22,
+            edu: 'High School',
+            colorFav: 'gray'
+        }
+    }
+    changeName = () => {
+        this.setState({
+            name: 'Draken',
+            age: 19,
+            edu: 'High School',
+            colorFav: 'blue'
+        });
     }
     render() {
-        return <h1>Hello I am {this.props.name}, My age {this.props.age} years</h1>
+        return (
+            <div>
+                <h1>Introduction</h1>
+                <p>
+                    Hello My name is {this.state.name},
+                    my old {this.state.age},
+                    i have studied at {this.state.edu},
+                    my color favorite is {this.state.colorFav} because the world in {this.state.colorFav == 'gray' ? 'grayscasle Now' : 'the blue is sky'}.
+                </p>
+                <button type='button' onClick={this.changeName}>Draken</button>
+            </div>
+        )
     }
 }
 export default Person;
