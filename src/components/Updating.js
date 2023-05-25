d2e29e3c47ca1a5d317120124c12cd40251328e5
@code <@code>
2023-05-26 05:00:18 +0700

    updatinggetDerivedStateFromProps

--- src/components/Updating.js
+++ src/components/Updating.js
@@ -1,1 +1,30 @@
-y
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class UpdatingComponents extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            favcol: 'gray'
+        }
+    }
+    static getDerivedStateFromProps(props, state) {
+        return { favcol: props.favoriteColor };
+    }
+    changeColor = () => {
+        this.setState({ favcol: 'blue' })
+    }
+    render() {
+        return (
+            <>
+                <h1>Upating Components</h1>
+                <h3>My color Favorite is {this.state.favcol}</h3>
+                <Button variant='dark' onClick={this.changeColor}>changeColor</Button>
+            </>
+        )
+    }
+}
+
+
+export default UpdatingComponents;
