8169f96532ca1d3e489b96de5a287ae635d17eba
@code <@code>
2023-05-28 03:56:22 +0700

    useState

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 function MyForm() {
+    const [name, setName] = useState("");
     return (
         <form>
             <label>Input Name:
-                <input type="text" />
+                <input type="text"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
             </label>
         </form>
 
