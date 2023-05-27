68d04e90f86b04876221c90d5e68eae39f743643
@code <@code>
2023-05-28 04:15:23 +0700

    hookuseState

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -3,14 +3,19 @@ import { useState } from "react";
 
 function MyForm() {
     const [name, setName] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Your submitted was ${name}`);
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Input Name:
                 <input type="text"
                     value={name}
                     onChange={e => setName(e.target.value)}
                 />
             </label>
+            <input type="submit" />
         </form>
 
     )
