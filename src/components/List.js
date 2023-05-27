b66d975240132781756cacddd375d9a601ed6b63
@code <@code>
2023-05-28 03:36:15 +0700

    ListWithKeys

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,23 @@
 import React from "react";
 
-function WordList(props){
+function WordList(props) {
   return <li> i am {props.name} </li>;
 }
 
-function List(){
-  const mhs = ["Mitsuya","Draken","Hina"];
-    return (
+function List() {
+  const mhs = [
+    { id: 0, name: "Mitsuya" },
+    { id: 1, name: "Draken" },
+    { id: 3, name: "Hina" }
+
+  ];
+  return (
     <div>
       <ul>
-        {mhs.map( x => <WordList name={x} />)}
+        {mhs.map(x => <WordList key={x.id} name={x.name} />)}
       </ul>
     </div>
-    );
+  );
 }
 
-export {List, WordList};
+export { List, WordList };
