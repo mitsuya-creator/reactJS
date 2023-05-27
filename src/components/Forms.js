6965d84f11b6e4effc1be465e485e5258046922e
@code <@code>
2023-05-28 03:47:10 +0700

    Forms

---
+++ src/components/Forms.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MyForm() {
+    return (
+        <form>
+            <label>Input Name:
+                <input type="text" />
+            </label>
+        </form>
+
+    )
+}
+
+export default MyForm;
