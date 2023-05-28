a8bad5ad1ab6cb48e070fad18ce655c3144a5727
code <code@code>
2023-05-28 18:49:37 +0000

    reactRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import MhsSelected from './components/Selected';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+import Layout from './pages/Layout';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import About from './pages/About';
+import NoPages from './pages/NoPages';
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="*" element={<NoPages />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MhsSelected />);
+root.render(<App />);
---
+++ src/pages/NoPages.js
@@ -0,0 +1,5 @@
+function NoPages(){
+  return <h1>404</h1>
+}
+
+export default NoPages;
