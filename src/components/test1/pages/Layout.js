15f822f862cbc0323c1478c88df3917fd4c0e0b1
code <code@code>
2023-05-29 17:43:13 +0000

    test1Layout

--- src/components/test1/pages/Layout.js
+++ src/components/test1/pages/Layout.js
@@ -1,1 +1,31 @@
-y
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import {Outlet, Link} from "react-router-dom";
+
+function Layout() {
+  return (
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>
+        <Link to="/">As Everything Unfolds</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav>
+            <Link to="/">Home</Link>
+            </Nav>
+            <Nav>
+            <Link to="/contact">Contact</Link>
+            </Nav>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import ContactMe from "./components/test1/pages/Contact";
 import Home from "./components/test1/pages/Home";
+import Layout from "./components/test1/pages/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home />);
+root.render(<Layout />);
