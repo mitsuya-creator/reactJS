140a5fffc3034a28c1c56f2e18e3c12fabf04b0f
@code <@code>
2023-05-30 06:47:12 +0700

    hookuseState

--- src/components/phase2/useState.js
+++ src/components/phase2/useState.js
@@ -1,1 +1,24 @@
-y
+import React from "react";
+import { useState } from "react";
+
+function Car() {
+    const [car, setCar] = useState({
+        brand: "Honda",
+        type: "Brio",
+        color: "gray"
+    })
+    const changeColor = () => {
+        setCar(x => {
+            return { ...x, color: "red" }
+        });
+    }
+    return (
+        <>
+            <h1>{car.brand}</h1>
+            <h1>{car.color}</h1>
+            <h1>{car.type}</h1>
+            <button onClick={changeColor}>change color</button>
+        </>
+    )
+}
+export default Car;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ContactMe from "./components/test1/pages/Contact";
-import Home from "./components/test1/pages/Home";
-import NavBar from "./components/test1/pages/Layout";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/style.css";
-
-export function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<NavBar />} >
-                    <Route index element={<Home />} />
-                    <Route path="/contact" element={<ContactMe />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-
-    )
-}
-
+import Car from "./components/phase2/useState";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Car />);
