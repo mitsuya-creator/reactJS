cf88e3855e33a9025f943bb289cd2fc8f993ed06
code <code@code>
2023-05-29 16:39:16 +0000

    test1Home

--- src/components/test1/pages/Home.js
+++ src/components/test1/pages/Home.js
@@ -1,1 +1,24 @@
-y
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import AEU from "../../../assets/AsEverythingUnfolds.jpg"
+
+
+function Home() {
+  return (
+    <Card className="body-card mx-2 my-1">
+      <Card.Img variant="top" src={AEU} />
+      <Card.Body>
+        <Card.Title>As Everything Unfolds</Card.Title>
+        <Card.Text>
+          As Everything Unfolds ist eine britische Rockband aus High Wycombe, Buckinghamshire, England.Wikipedia (DE)
+        </Card.Text>
+        <Button variant="primary">
+          <a href="https://www.aseverythingunfolds.com/" className="text-white text-decoration-none">Website</a>
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ContactMe from "./components/test1/pages/Contact";
+import Home from "./components/test1/pages/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<ContactMe />);
+root.render(<Home />);
