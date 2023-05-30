07d8e401cf37d9957553bb6c275c2f4f9d5dfc77
code <code@code>
2023-05-30 17:14:44 +0000

    hookuseRef

--- src/components/phase2/UseRef.js
+++ src/components/phase2/UseRef.js
@@ -4,16 +4,14 @@ import {useState, useEffect, useRef} from "react";
 
 function Ref(){
   const [inputValue, setInputValue] = useState("");
-  const count = useRef(0);
-  const el = useRef();
-  const focusEl = () => {
-    el.current.focus();
-  }
-  useEffect(() =>  {count.current = count.current + 1});
+  const prevValue = useRef("");
+  useEffect(() => {
+    prevValue.current = inputValue;
+  },[inputValue])
   return (
     <>
-      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} ref={el}/>
-      <h1 onClick={focusEl}>Render count {count.current}</h1>
+      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+      <h1>prevValue {prevValue.current}</h1>
     </>
     )
 }
