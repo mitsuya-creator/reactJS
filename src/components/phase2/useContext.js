7959bbb669f89bba784d9b49020d1f56aac84f4c
code <code@code>
2023-05-30 15:48:18 +0000

    hookuseContext

--- src/components/phase2/useContext.js
+++ src/components/phase2/useContext.js
@@ -1,25 +1,30 @@
 import React from "react";
-import {useState} from "react";
+import {useState, createContext, useContext} from "react";
 
+const UserContext = createContext();
 function Comp1(){
   const [user, setUser] = useState("Mitsuya");
   return (
     <>
+    <UserContext.Provider value={user}>
       <h1>{`Hello ${user}`}</h1>
       <Comp2 user={user} />
+     </UserContext.Provider>
     </>
     )
 }
-function Comp2({user}){
+function Comp2(){
+  const user = useContext(UserContext);
   return (
     <>
-    <h1>Comp2</h1>
-    <Comp3 user={user} />
+    <h1>Comp2 {user}</h1>
+    <Comp3/>
     </>
     )
 }
 
-function Comp3({user}){
+function Comp3(){
+  const user = useContext(UserContext);
   return (
     <h1>{`hello again ${user}`}</h1>
     )
