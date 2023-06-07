2c1322c495bc4266a3208026e8f406cbb4c4c735
code <jj4068851@gmail.com>
2023-06-07 20:15:57 +0000

    updateStateReverseSort

--- src/index.js
+++ src/index.js
@@ -1,55 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client.js';
+import ReactDOM from 'react-dom/client';
 import { useState } from 'react';
-import Background from './reactDev/Background.js';
-import Box from './reactDev/Box.js';
+import  AddArtist from './reactDev/addArray';
 
-const initialPosition = {
-  x: 0,
-  y: 0
-};
 
-export default function Canvas() {
-  const [shape, setShape] = useState({
-    color: 'orange',
-    position: initialPosition
-  });
-
-  function handleMove(dx, dy) {
-    setShape({ ...shape, position: { x: shape.position.x + dx, y: shape.position.y + dy } });
-    console.log(shape.position.x, shape.position.y);
-    console.log(dx, dy);
-  }
-
-  function handleColorChange(e) {
-    setShape({
-      ...shape,
-      color: e.target.value
-    });
-  }
-
-  return (
-    <>
-      <select
-        value={shape.color}
-        onChange={handleColorChange}
-      >
-        <option value="orange">orange</option>
-        <option value="lightpink">lightpink</option>
-        <option value="aliceblue">aliceblue</option>
-      </select>
-      <Background
-        position={initialPosition}
-      />
-      <Box
-        color={shape.color}
-        position={shape.position}
-        onMove={handleMove}
-      >
-        Drag me!
-      </Box>
-    </>
-  );
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Canvas />)
+root.render(<AddArtist />)
--- src/reactDev/addArray.js
+++ src/reactDev/addArray.js
@@ -12,9 +12,22 @@ export default function AddArtist(){
       }/>
       </label>
       <button type="button" onClick={() => {
-        SetArtist([{id:index++, name: name}, ...artist]);
+        SetArtist([...artist,{id:index++, name: name}]);
         }
       }>Add</button>
+      <button type="button" onClick={() => {
+        const reverseList = [...artist];
+        reverseList.reverse();
+        console.log('reverse', reverseList);
+        SetArtist(reverseList);
+      }}>Reverse</button>
+      <button type="button" onClick={()=> {
+        const sortList = [...artist];
+        sortList.sort((a,b) => a.name > b.name ? 1 : -1);
+        console.log('sort', sortList);
+        SetArtist(sortList);
+      }
+      }>Sort</button>
       <ul>
       {artist.map(index => <li key={index.id}>{index.name}      <button type="button" onClick={() => {
         SetArtist(artist.filter(a => a.id !== index.id));
