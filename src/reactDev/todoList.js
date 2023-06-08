2372ef3155b204025531200eb4f0391ba2ea16dc
@code <jj4068851@gmail.com>
2023-06-08 07:00:11 +0700

    todoList

---
+++ src/reactDev/todoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
+let setId = 0;
+function AddList() {
+    const [todo, setTodo] = useState("");
+    const [list, setList] = useState([]);
+    return (
+        <>
+            <h1>Todo List</h1>
+            <input type="text" onChange={e => setTodo(e.target.value)} />
+            <button type="button" onClick={() => {
+                setList([...list, { id: setId++, task: todo }]);
+            }}>Add</button>
+            <ul>
+                {list.map(content => <li key={content.id}>{content.task}<button type="button" onClick={() => setList(list.filter(idContent => idContent.id !== content.id))}>Delete</button></li>)}
+            </ul>
+            {console.log(list)}
+        </>
+    )
+}
+
+export default AddList;
