3612796b880c56a84b8bdd3f56bff665b38a4bc1
code <jj4068851@gmail.com>
2023-06-09 00:37:20 +0000

    editBtn

--- src/reactDev/todoList.js
+++ src/reactDev/todoList.js
@@ -5,16 +5,29 @@ let setId = 0;
 function AddList() {
     const [todo, setTodo] = useState("");
     const [list, setList] = useState([]);
+    const addBtn = () => {
+      if(todo === "") alert("inputSomeText")
+      else{
+        setList([...list,{id: setId++, task: todo}]);
+        setTodo("");
+      }
+    }
+    function delBtn(id) {
+      setList(list.filter(idContent => idContent.id !== id));
+    }
+    function editBtn(id) {
+      let x = prompt("newTask");
+      const newTask = [...list];
+      newTask[id].task = x;
+      setList(newTask);
+    }
     return (
         <>
             <h1>Todo List</h1>
             <input type="text" value={todo} onChange={e => setTodo(e.target.value)} />
-            <button type="button" onClick={() => {
-                setList([...list, { id: setId++, task: todo }]);
-                setTodo("");
-            }}>Add</button>
+            <button type="button" onClick={addBtn}>Add</button>
             <ul>
-                {list.map(content => <li key={content.id}>{content.task}<button type="button" onClick={() => setList(list.filter(idContent => idContent.id !== content.id))}>Delete</button></li>)}
+                {list.map(content => <li key={content.id}>{content.task}<button type="button" onClick={() => delBtn(content.id)}>Delete</button><button type="button" onClick={() => editBtn(content.id)}>Edit</button></li>)}
             </ul>
             {console.log(list)}
             {console.log(todo)}
