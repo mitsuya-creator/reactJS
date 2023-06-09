b6f32e659f705478397168d6760de4fd9007fa49
code <jj4068851@gmail.com>
2023-06-09 19:16:42 +0000

    updateStateArray

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AddList from './reactDev/todoList';
+import ShoppingCart from './test';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AddList />)
+root.render(<ShoppingCart />)
--- src/test.js
+++ src/test.js
@@ -1,28 +1,68 @@
+import React from "react";
 import { useState } from 'react';
 
-export default function RequestTracker() {
-    const [pending, setPending] = useState(0);
-    const [completed, setCompleted] = useState(0);
-    function handleClick() {
-        setPending(pending => pending + 1);
-        setTimeout(() => {
-            setPending(pending => pending - 1);
-            setCompleted(completed => completed + 1);
-        }, 3000);
-    }
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
 
-    return (
-        <>
-            <h3>
-                Pending: {pending}
-            </h3>
-            <h3>
-                Completed: {completed}
-            </h3>
-            <button onClick={handleClick}>
-                Buy
-            </button>
-        </>
-    );
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+    setProducts(products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count + 1
+        };
+      } else {
+        return product;
+      }
+    }))
+  }
+  function handleDecrease(productS){
+      let nextProduct = products.map(product => {
+        if(product.id === productS.id){
+          return {...product, count: product.count - 1};
+        }else{
+          return product;
+        }
+      });
+      nextProduct = nextProduct.filter(product => product.count > 0);
+      setProducts(nextProduct);
+  }
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          {product.name}
+          {' '}
+          (<b>{product.count}</b>)
+          <button onClick={() => {
+            handleIncreaseClick(product.id);
+          }}>
+            +
+          </button>
+          <button onClick={() => handleDecrease(product)}>
+            –
+          </button>
+        </li>
+      ))}
+      {console.log(products)}
+    </ul>
+  );
 }
-
