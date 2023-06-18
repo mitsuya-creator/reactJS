7efb1b6daf9179c522fd595600960b32ec41c8d9
@code <jj4068851@gmail.com>
2023-06-19 06:10:53 +0700

    handleOnChange

--- src/reactDev/listPacket.js
+++ src/reactDev/listPacket.js
@@ -1,13 +1,26 @@
 import React from "react";
 
-export default function ListPacket({packet,setPacket}){
+export default function ListPacket({ packet, setPacket }) {
+  const handleOnChange = (item, e) => {
+    setPacket(packet.map(id => {
+      if (id.id === item.id) {
+        return { ...item, packed: e.target.checked };
+      } else {
+        return { ...id };
+      }
+    }));
+  }
   return (
     <>
-       <ul>
-          {packet.map(item => <li key={item.id}>{item.title}<button type="button" onClick={() => setPacket(packet.filter(id => item.id !== id.id)) }>Delete</button>
-            </li>
-          )}
-       </ul>
+      <ul>
+        {packet.map(item => <li key={item.id}>
+          <input type="checkbox" checked={item.packed} onChange={(e) => handleOnChange(item, e)} />
+          {item.title}
+          <button type="button" onClick={() => setPacket(packet.filter(id => item.id !== id.id))}>Delete</button>
+        </li>
+        )}
+      </ul>
+      {console.log(packet)}
     </>
-    )
+  )
 }
