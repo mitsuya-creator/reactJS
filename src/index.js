46065d00e6b5efa4162e6ce2de73980631c75889
@code <jj4068851@gmail.com>
2023-06-23 06:31:22 +0700

    filterItemsIncludesSpaceChar

--- src/index.js
+++ src/index.js
@@ -8,26 +8,27 @@ import mhs from './reactDev/data';
 export default function App() {
   const [keyword, setKeyword] = useState("");
   const result = filterItems(mhs, keyword);
+  result.map(item => item.name.split())
   return (
     <>
       <SearchBar
         word={keyword}
         setWord={setKeyword}
       />
-      {console.log(keyword)}
+      {/* {console.log(keyword)} */}
       <ListMhs items={result} query={keyword} />
-      {console.log(result)}
+      {/* {console.log(result)} */}
     </>
   )
 }
-
 function ListMhs({ items, query }) {
   return (
     <table>
       <tbody>
         {items.map(content => <tr key={content.id}>
-          <td>{content.name.split("").map((character, idx) => character === query.toUpperCase() || character === query ? <span key={idx} style={{ color: "red" }}>{character}</span> : <span key={idx}>{character}</span>
-          )
+          <td>{content.name.split()
+            // .map((character, idx) => character === query.toUpperCase() || character === query ? <span key={idx} style={{ color: "red" }}>{character}</span> : <span key={idx}>{character}</span>
+            // )
             // .forEach(character => character === query ? <span style={{ color: "red" }}>{character}</span> : { character })
           }</td>
           <td>{content.quote}</td>
--- src/reactDev/filterItems.js
+++ src/reactDev/filterItems.js
@@ -1,7 +1,11 @@
 function filterItems(items, query) {
     query = query.toLowerCase();
-    return items.filter(item => item.name.split(" ")
-        .some(word => word.toLowerCase().startsWith(query)));
+    return items.filter(item => {
+        let name;
+        query.includes(" ") ? name = item.name.split() : name = item.name.split(" ");
+        return name.some(word => word.toLowerCase().startsWith(query))
+    }
+    );
 }
 
 export default filterItems;
