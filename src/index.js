9a0c13844dfee35159c3cb083933d62d374ae22e
@code <jj4068851@gmail.com>
2023-06-25 05:29:46 +0700

    keepStateValue

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import Counter from './reactDev/counter';
-import { useState } from 'react';
-
-export default function App() {
-  const [show, setShow] = useState(false);
-  return (
-    <>
-      <Counter />
-      {show ? <Counter /> : null}
-      <input type='checkbox' checked={show} onChange={e => setShow(e.target.checked)} />
-      <span>Show second counter</span>
-    </>
-  )
-=======
 import TextField from './reactDev/hint';
 
 export default function App() {
@@ -22,8 +7,7 @@ export default function App() {
     <>
       <TextField />
     </>
-    )
->>>>>>> a0dd0dc8a0d6e9b80fdefbe9a9234151d2fd0c9e
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
