472c46449958c8cfd59c7368461164658eba62c9
@code <jj4068851@gmail.com>
2023-06-29 01:49:44 +0700

    CRUtaskList

--- src/reactDev/editTasks.js
+++ src/reactDev/editTasks.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 
-export default function EditTask({ tasks, setTasks, taskUpdate, setEdit }) {
+export default function EditTask({ tasks, setTasks, taskUpdate, id }) {
+    let value;
     const [text, setText] = useState("");
+    const [reset, setReset] = useState(false);
     const handleSave = id => {
         let updateTasks = tasks.map(task => {
-            if (tasks.id === id) {
-                return { ...taskUpdate, title: text }
+            if (task.id === id) {
+                console.log(id);
+                return { ...taskUpdate, title: value, id: value, isEdit: false }
             } else {
                 return task;
             }
         })
         setTasks(updateTasks);
-        setEdit(() => false);
+    }
+    if (reset) {
+        value = taskUpdate.title;
+    } else {
+        value = text;
     }
     return (
         <>
-            <input type="text" onChange={e => setText(e.target.value)} />
-            <button type="button" onClick={handleSave(taskUpdate.id)}>Save</button>
+            <input type="text" onChange={e => setText(e.target.value)} value={value} disabled={
+                reset === true
+            } />
+            <button type="button" onClick={() => handleSave(id)} disabled={value === ""}>Save</button>
+            <button type="button" onClick={() => {
+                setText("");
+                setReset(!reset);
+            }}  >{reset ? "Edit" : "Reset"}</button >
         </>
     )
 }
--- src/reactDev/listTasks.js
+++ src/reactDev/listTasks.js
@@ -1,10 +1,5 @@
-
-// Note: 
-// add feature delete task
-// add Feature if task already exist the save button and add button  will not be activated!
-
-
-import React, { useState } from "react";
+import React from "react";
+import EditTask from "./editTasks";
 
 export default function ListTask({ tasks, setTasks }) {
     const handleChange = (t, event) => {
@@ -42,41 +37,6 @@ export default function ListTask({ tasks, setTasks }) {
                     }
                 </li>)}
             </ul>
-        </>
-    )
-}
-
-
-function EditTask({ tasks, setTasks, taskUpdate, id }) {
-    let value;
-    const [text, setText] = useState("");
-    const [reset, setReset] = useState(false);
-    const handleSave = id => {
-        let updateTasks = tasks.map(task => {
-            if (task.id === id) {
-                console.log(id);
-                return { ...taskUpdate, title: value, id: value, isEdit: false }
-            } else {
-                return task;
-            }
-        })
-        setTasks(updateTasks);
-    }
-    if (reset) {
-        value = taskUpdate.title;
-    } else {
-        value = text;
-    }
-    return (
-        <>
-            <input type="text" onChange={e => setText(e.target.value)} value={value} disabled={
-                reset === true
-            } />
-            <button type="button" onClick={() => handleSave(id)} disabled={value === ""}>Save</button>
-            <button type="button" onClick={() => {
-                setText("");
-                setReset(!reset);
-            }}  >{reset ? "Edit" : "Reset"}</button >
         </>
     )
 }
