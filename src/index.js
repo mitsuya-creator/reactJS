025e68f6a73b065ffdc032b37c35e2a5284ad0de
@code <jj4068851@gmail.com>
2023-07-09 08:56:18 +0700

    addSymbolChar

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { useReducer, useState } from 'react';
 import taskReducer from './reactDev/taskWithReducer';
 import SearchTask from './reactDev/searchTasks';
 import filterTasks from './reactDev/filterTasks';
-import iconSearch from '../src/assets/search.svg';
 import './css/style.css';
 
 let keyID = 0;
@@ -79,17 +78,15 @@ export default function App() {
   return (
     <>
       <h1>TodoList App</h1>
-      <label for="language">Bahasa</label>
-      <select name="language" id="language">
-        <option>English</option>
-        <option>Indonesia</option>
-      </select>
       {
         addTodo ? <AddTasks onAddBTn={handleAddTasks} setText={setText} text={text} /> : <button type="button" onClick={() => {
           setAddTodo(true)
           setSearchTodo(false)
           setKeyword("")
-        }}>Create Tasks</button>
+        }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+          </svg></button>
 
       }
       {
@@ -97,7 +94,9 @@ export default function App() {
           setSearchTodo(true)
           setAddTodo(false)
           setText("")
-        }}><img src={iconSearch} alt='search-icon' /></button>
+        }}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg></button>
       }
       <ListTask tasks={result} onChangeTask={handleChanged} onDeleted={handleDeleted} allTasks={tasks} />
       {console.log(tasks)}
--- src/reactDev/listTasks.js
+++ src/reactDev/listTasks.js
@@ -21,14 +21,19 @@ function Task({ list, onChangeTask, onDeleted }) {
         taskContent = (
             <>
                 <input type="text" value={list.title} onChange={e => onChangeTask({ ...list, title: e.target.value })} />
-                <button type="button" onClick={() => setIsEdit(false)} >Save</button>
+                <button type="button" onClick={() => setIsEdit(false)} ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
+                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z" />
+                </svg></button >
             </>
         )
     } else {
         taskContent = (
             <>
                 {list.title}
-                <button type="button" onClick={() => setIsEdit(true)}>Edit</button>
+                <button type="button" onClick={() => setIsEdit(true)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                </svg></button>
             </>
         )
     }
@@ -37,7 +42,9 @@ function Task({ list, onChangeTask, onDeleted }) {
             <label>
                 <input type="checkbox" checked={list.done} onChange={e => onChangeTask({ ...list, done: e.target.checked })} />
                 {taskContent}
-                <button type="button" onClick={() => onDeleted(list.id)} >Delete</button>
+                <button type="button" onClick={() => onDeleted(list.id)} ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                </svg></button>
             </label>
         </>
     )
