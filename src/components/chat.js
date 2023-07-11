f5dea30c6f0262ff6fc7926869e930b30a6f5edb
@code <jj4068851@gmail.com>
2023-07-11 08:46:02 +0700

    chatField

---
+++ src/components/chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TextField() {
+    return (
+        <>
+            <section>
+                <textarea />
+                <button type="button">send to xxxx</button>
+            </section>
+        </>
+    )
+}
---
+++ src/features/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TextField from "../components/chat";
+
+export default function App() {
+    return (
+        <>
+            <TextField />
+        </>
+    )
+}
