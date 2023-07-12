1f6e1ac7fe0a1fe67ae0ee50819af0dd4106d817
@code <jj4068851@gmail.com>
2023-07-12 10:03:43 +0700

    dataForContext

---
+++ src/utils/data.js
@@ -0,0 +1,9 @@
+import imgMitsuya from "../assets/Mitsuya.jpeg";
+import imgChifuyu from "../assets/cifuyu.jpg";
+import imgHina from "../assets/hina.jpg";
+
+export const mhs = [
+    { id: 0, name: "Mitsuya Takashi", imgUrl: imgMitsuya },
+    { id: 1, name: "Chifuyu Matsuna", imgUrl: imgChifuyu },
+    { id: 2, name: "Tachibana Hina", imgUrl: imgHina }
+]
